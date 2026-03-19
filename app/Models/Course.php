d3d8d8e706e7bcc422432016de0f98aabc6e8009
Neraj Lal <mac@macs-MacBook-Air.local>
2026-03-19 13:08:52 +0530

    work started views

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'thumbnail', 'price', 'instructor_name'];
+
+    public function lessons()   { return $this->hasMany(Lesson::class); }
+    public function batches()   { return $this->hasMany(Batch::class); }
+    public function admissions(){ return $this->hasMany(Admission::class); }
+    public function enrollments(){ return $this->hasMany(Enrollment::class); }
 }
